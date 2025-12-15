dc6effd1dd8f776286f9d643f2da9b48610fa9ea
Le Minh Vu <leminhvu9124@gmail.com>
2025-12-15 22:00:33 +0700

    asd

--- index.php
+++ index.php
@@ -1,16 +1,3 @@
 <?php
-// Cho phép mọi trang web (Frontend) đều có thể gọi tới Backend này (CORS)
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-
-// Dữ liệu giả lập (Database)
-$data = [
-    "status" => "success",
-    "message" => "Xin chào! Đây là dữ liệu từ Backend PHP",
-    "student" => "Lê Minh Vũ",
-    "time" => date("Y-m-d H:i:s")
-];
-echo "heheh";
-// Trả dữ liệu về
-echo json_encode($data);
+echo"php dom dom yes yes"
 ?>
